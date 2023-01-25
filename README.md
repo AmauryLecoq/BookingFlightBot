ae67ae4fc812cd5364ee237ccec95fd8b2e55e9d
amaurylecoq <amaury.lecoq@outlook.com>
2023-01-25 12:03:27 +0100

    add badge to readme add test 2 to test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ChatBot for BookingFlight based on 21.corebot-app-insights
+
+
+![Tests](https://github.com/AmauryLecoq/BookingFlightBot/actions/workflows/master_flightbookingbot.yml/badge.svg)
--- tests/test_1.py
+++ tests/test_1.py
@@ -42,9 +42,11 @@ from flight_booking_recognizer import FlightBookingRecognizer
 from booking_details import BookingDetails
 
 
+
+
 class BookingDialogTest(aiounittest.AsyncTestCase):
 
-    async def test_booking_dialog(self):
+    async def test_booking_dialog_1(self):
         async def exec_test(turn_context: TurnContext):
             #need to set dialog context
             dialog_context = await dialogs.create_context(turn_context)
@@ -86,5 +88,47 @@ class BookingDialogTest(aiounittest.AsyncTestCase):
             f"Please confirm, I have you traveling to: Paris"
             f" from: Lille on: 2023-01-01."
             f" Returning on: 2023-01-31 with a budget of : 1000 ."
+            f" (1) Yes or (2) No")
+    
+    async def test_booking_dialog_2(self):
+        async def exec_test(turn_context: TurnContext):
+            #need to set dialog context
+            dialog_context = await dialogs.create_context(turn_context)
+            results = await dialog_context.continue_dialog()
+            if (results.status == DialogTurnStatus.Empty):
+                await dialog_context.begin_dialog(dialog_id)
+            elif results.status == DialogTurnStatus.Complete:
+                reply =  results.result
+                await turn_context.send_activity(reply)
+            
+            await conv_state.save_changes(turn_context)
+
+        #need to create object for the test adapter
+        adapter = TestAdapter(exec_test)
+        # for dialog state u need converstation state
+        conv_state = ConversationState(MemoryStorage())
+        booking_details = BookingDetails()
+        dialog_id = MainDialog.__name__
+        config = DefaultConfig()
+        luis_recognizer = FlightBookingRecognizer(config)
+        booking_dialog = BookingDialog()
+        
+        #for dialog set u need dialoag state
+        dialogs_state = conv_state.create_property("dialog_state")
+        dialogs = DialogSet(dialogs_state)
+        dialogs.add(MainDialog(luis_recognizer,booking_dialog ))
+        #test adapter will be use to pass all information
+        
+
+
+        step1 = await adapter.test("hi", "What can I help you with today?") # call adapter to ask
+        step2 = await step1.send(
+            "yes, how about going to neverland from caprica on august 13,\
+             2016 and back on february 02, 2017 for 5 adults. \
+                for this trip, my budget would be 1900.") # input to the adapter
+        await step2.assert_reply(
+            f"Please confirm, I have you traveling to: Neverland"
+            f" from: Caprica on: 2016-08-13."
+            f" Returning on: 2017-02-02 with a budget of : 1900 ."
             f" (1) Yes or (2) No")
             
